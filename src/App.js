7a2b98dc8c1c98b28234e05b4f5a28394031ca1d
katty-ptk <katy.lazar05@g,ail.com>
2023-12-13 16:11:55 +0200

    ceva text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>shopPal React App</h1>
+      <p>In fisierul 
+        <span
+          style={{
+            textDecoration: "underline",
+            fontWeight: "bold"
+          }}
+        > src/App.js </span> 
+        o sa lucrezi deocamdata</p>
     </div>
   );
 }
